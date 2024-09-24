46b3a5e6a67d61ad7f9d7d3e88012fb09491134f
Irisation23 <bunsung0345@gmail.com>
2024-09-24 20:53:14 +0900

    Title: 건너뛰고 별표 출력하기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240924/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) {
+        int n = nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            System.out.println();
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
